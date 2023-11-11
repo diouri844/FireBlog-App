3dc67a3a079626727b407e27f4bfb812bc500ddd
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-11 18:30:47 +0100

    Setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Ref, ref } from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+
+const message:Ref<string> = ref("Fire Blog Posts app with vue/pina and firebase");
+
+
+</script>
+
+<template>
+  <HelloWorld :msg="message" />
+</template>
+
+
