a907d9c797c961bc68fdd88442b82d973e3a6a99
Chopen <75852177+diouri844@users.noreply.github.com>
2023-11-11 19:56:00 +0100

    Setup router

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './providers/router.provider'
 
-createApp(App).mount('#app')
+// import configured router : 
+
+
+
+
+createApp(App)
+.use(router)
+.mount('#app');
